dbabd7a035a56e455addf4aceadcda58cc13a110
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2016-08-07 18:42:26 +0200

    Add locale

--- main/java/fr/redrelay/lockablemod/items/LockableModItem.java
+++ main/java/fr/redrelay/lockablemod/items/LockableModItem.java
@@ -6,7 +6,7 @@ import net.minecraft.item.Item;
 
 public class LockableModItem extends Item{
 
-	private static final CreativeTabs LOCKABLE_CREATIVE_TAB = new CreativeTabs(LockableMod.MODNAME) {
+	private static final CreativeTabs LOCKABLE_CREATIVE_TAB = new CreativeTabs(LockableMod.MODID) {
 		
 		@Override
 		public Item getTabIconItem() {
